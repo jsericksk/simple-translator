5f566d3f855a3a0ba03315cce162e5225fda3a53
jsericksk <jsericksk@gmail.com>
2020-03-04 01:21:50 -0300

    Improvements

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter text to translate: ");
+		System.out.println("Enter text: ");
 		String textToTranslate = input.nextLine();
 		String translatedText = Translator.translate(textToTranslate, Language.AUTO_DETECT, Language.ENGLISH);
 		System.out.println("Translated text: " + translatedText);
